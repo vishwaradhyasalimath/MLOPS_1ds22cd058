de532910a2b10bb9ef5d704de6fcb642caa9df29
vishwaradhya <vishwasalimath7@gmail.com>
2025-05-18 22:58:04 +0530

    api.py

---
+++ API.py
@@ -0,0 +1,15 @@
+# Save as predict_api.py
+from flask import Flask, request, jsonify
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
+app = Flask(__name__)
+X, y = load_iris(return_X_y=True)
+model = LogisticRegression(max_iter=200).fit(X, y)
+@app.route("/predict", methods=['POST'])
+def predict():
+    data = request.json['features']
+    prediction = model.predict([data])
+    return jsonify({'prediction': int(prediction[0])})
+if __name__ =='__main__':
+    app.run(debug=True)
